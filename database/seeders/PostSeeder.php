e73b2d4c1b3fc5eb28b49e748f6545d91506f615
lauracolladoq <laucolqui@gmail.com>
2024-05-08 20:55:38 +0200

    Creación de Seeders

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,44 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Creación de Posts almacenados en una variable para asignarlos tags y likes de forma aleatoria
+        $posts = Post::factory(30)->create();
+
+        foreach ($posts as $item) {
+            $item->tags()->attach($this->getIdTagsRandom());
+            $item->usersLikes()->attach(self::getArrayLikes());
+        }
+    }
+
+    // Obtiene un array con todos los Tags y los almacena de forma aleatoria como arrays en un nuevo array
+    private function getIdTagsRandom(): array
+    {
+        $tags = [];
+
+        $arrayTags = Tag::pluck('id')->toArray();
+        $indices = (array) array_rand($arrayTags, random_int(1, count($arrayTags)));
+        foreach ($indices as $indice) {
+            $tags[] = $arrayTags[$indice];
+        }
+        return $tags;
+    }
+
+    // Obtiene un array con todos los Likes de un usuario y y los almacena de forma aleatoria como arrays en un nuevo array
+    private static function getArrayLikes(): array
+    {
+        $likes = [];
+        $usersId = User::pluck('id')->toArray();
+        $indices = array_rand($usersId, random_int(1, count($usersId)));
+
+         // Si solo se elige un índice, se devuelve como array
+        if (!is_array($indices)) {
+            return [$usersId[$indices]];
+        }
+
+        foreach ($indices as $indice) {
+            $likes[] = $usersId[$indice];
+        }
+
+        return $likes;
     }
 }
--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,25 @@ class TagSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tags = [
+            'Animals' => "#FF0000",
+            'Art' => "#FF6200",
+            'Movies' => "#FFC300",
+            'Food' => "#BFE100",
+            'Sports' => "#7FFF00",
+            'Music' => "#55CF7F",
+            'Fashion' => "#1E90FF",
+            'Nature' => "#5530D0",
+            'Technology' => "#8A2BE2",
+            'Travel' => "#B75F8D",
+            'Video Games' => "#FF1493"
+        ];
+
+        foreach ($tags as $n => $c) {
+            Tag::create([
+                'name' => $n,
+                'color' => $c
+            ]);
+        }
     }
 }
