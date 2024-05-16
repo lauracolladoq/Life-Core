f6a730dd057874eb17e3ed538301511fd1c799b4
lauracolladoq <laucolqui@gmail.com>
2024-05-16 20:23:46 +0200

    Cambio de diseño y funcionalidad de Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,139 @@
 <x-guest-layout>
     <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
-        <x-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-label for="terms">
-                        <div class="flex items-center">
-                            <x-checkbox name="terms" id="terms" required />
-
-                            <div class="ms-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
+        <div
+            class="grid md:grid-cols-2 items-center gap-4 max-w-6xl w-full p-4 m-4 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md">
+            <div class="md:max-w-md w-full sm:px-6 py-4">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="mb-5">
+                        <h3 class="text-3xl font-extrabold">Create an account</h3>
+                    </div>
+                    <div class="mt-4">
+                        <div class="flex items-start mb-2">
+                            <x-label for="email" class="text-xs block" value="{{ __('Email') }}" />
+                            <x-validation-errors for="email" class="ml-4 text-xs" />
                         </div>
-                    </x-label>
-                </div>
-            @endif
+                        <x-input id="email"
+                            class="w-full text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none"
+                            type="email" name="email" :value="old('email')" required autocomplete="username"
+                            placeholder="Enter email" />
+                    </div>
+                    <div class="mt-4">
+                        <div class="flex items-start mb-2">
+                            <x-label for="name" class="text-xs block" value="{{ __('Name') }}" />
+                            <x-validation-errors for="name" class="ml-4 text-xs" />
+                        </div>
+                        <x-input id="name"
+                            class="w-full text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none"
+                            type="text" name="name" :value="old('name')" required autofocus autocomplete="name"
+                            placeholder="Enter name" />
+                    </div>
+                    <div class="mt-4">
+                        <div class="flex items-start mb-2">
+                            <x-label for="username" class="text-xs block" value="{{ __('Username') }}" />
+                            <x-validation-errors for="username" class="ml-4 text-xs" />
+                        </div>
+                        <x-input id="username"
+                            class="w-full text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none"
+                            type="text" name="username" :value="old('username')" required autofocus autocomplete="name"
+                            placeholder="Enter username" />
+                    </div>
+                    <div class="mt-4">
+                        <div class="flex items-start mb-2">
+                            <x-label for="password" class="text-xs block" value="{{ __('Password') }}" />
+                            <x-validation-errors for="password" class="ml-4 text-xs" />
+                        </div>
+                        <div class="relative flex items-center">
+                            <x-input id="password"
+                                class="w-full text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none"
+                                type="password" name="password" required autocomplete="new-password"
+                                placeholder="Enter password" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb"
+                                class="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
+                                <path
+                                    d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+                                    data-original="#000000"></path>
+                            </svg>
+                        </div>
+                    </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ms-4">
-                    {{ __('Register') }}
-                </x-button>
+                    <div class="mt-4">
+                        <div class="flex items-start mb-2">
+                            <x-label for="password_confirmation" class="text-sm block"
+                                value="{{ __('Confirm Password') }}" />
+                            <x-validation-errors for="password" class="ml-4 text-xs" />
+                        </div>
+                        <div class="relative flex items-center">
+                            <x-input id="password_confirmation"
+                                class="w-full text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none"
+                                type="password" name="password_confirmation" required autocomplete="new-password"
+                                placeholder="Enter password" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb"
+                                stroke="#bbb" class="w-[18px] h-[18px] absolute right-2 cursor-pointer"
+                                viewBox="0 0 128 128">
+                                <path
+                                    d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+                                    data-original="#000000"></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="mt-8 flex justify-center">
+                        <x-button class="w-80">
+                            {{ __('Register') }}
+                        </x-button>
+                    </div>
+                    <p class="my-3 text-sm text-gray-400 text-center">or continue with</p>
+                    <div class="space-x-8 flex justify-center">
+                        <button type="button" class="border-none outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30px" class="inline" viewBox="0 0 512 512">
+                                <path fill="#fbbd00"
+                                    d="M120 256c0-25.367 6.989-49.13 19.131-69.477v-86.308H52.823C18.568 144.703 0 198.922 0 256s18.568 111.297 52.823 155.785h86.308v-86.308C126.989 305.13 120 281.367 120 256z"
+                                    data-original="#fbbd00" />
+                                <path fill="#0f9d58"
+                                    d="m256 392-60 60 60 60c57.079 0 111.297-18.568 155.785-52.823v-86.216h-86.216C305.044 385.147 281.181 392 256 392z"
+                                    data-original="#0f9d58" />
+                                <path fill="#31aa52"
+                                    d="m139.131 325.477-86.308 86.308a260.085 260.085 0 0 0 22.158 25.235C123.333 485.371 187.62 512 256 512V392c-49.624 0-93.117-26.72-116.869-66.523z"
+                                    data-original="#31aa52" />
+                                <path fill="#3c79e6"
+                                    d="M512 256a258.24 258.24 0 0 0-4.192-46.377l-2.251-12.299H256v120h121.452a135.385 135.385 0 0 1-51.884 55.638l86.216 86.216a260.085 260.085 0 0 0 25.235-22.158C485.371 388.667 512 324.38 512 256z"
+                                    data-original="#3c79e6" />
+                                <path fill="#cf2d48"
+                                    d="m352.167 159.833 10.606 10.606 84.853-84.852-10.606-10.606C388.668 26.629 324.381 0 256 0l-60 60 60 60c36.326 0 70.479 14.146 96.167 39.833z"
+                                    data-original="#cf2d48" />
+                                <path fill="#eb4132"
+                                    d="M256 120V0C187.62 0 123.333 26.629 74.98 74.98a259.849 259.849 0 0 0-22.158 25.235l86.308 86.308C162.883 146.72 206.376 120 256 120z"
+                                    data-original="#eb4132" />
+                            </svg>
+                        </button>
+                        <button type="button" class="border-none outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30px" fill="#000"
+                                viewBox="0 0 22.773 22.773">
+                                <path
+                                    d="M15.769 0h.162c.13 1.606-.483 2.806-1.228 3.675-.731.863-1.732 1.7-3.351 1.573-.108-1.583.506-2.694 1.25-3.561C13.292.879 14.557.16 15.769 0zm4.901 16.716v.045c-.455 1.378-1.104 2.559-1.896 3.655-.723.995-1.609 2.334-3.191 2.334-1.367 0-2.275-.879-3.676-.903-1.482-.024-2.297.735-3.652.926h-.462c-.995-.144-1.798-.932-2.383-1.642-1.725-2.098-3.058-4.808-3.306-8.276v-1.019c.105-2.482 1.311-4.5 2.914-5.478.846-.52 2.009-.963 3.304-.765.555.086 1.122.276 1.619.464.471.181 1.06.502 1.618.485.378-.011.754-.208 1.135-.347 1.116-.403 2.21-.865 3.652-.648 1.733.262 2.963 1.032 3.723 2.22-1.466.933-2.625 2.339-2.427 4.74.176 2.181 1.444 3.457 3.028 4.209z"
+                                    data-original="#000000"></path>
+                            </svg>
+                        </button>
+                        <button type="button" class="border-none outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30px" fill="#007bff"
+                                viewBox="0 0 167.657 167.657">
+                                <path
+                                    d="M83.829.349C37.532.349 0 37.881 0 84.178c0 41.523 30.222 75.911 69.848 82.57v-65.081H49.626v-23.42h20.222V60.978c0-20.037 12.238-30.956 30.115-30.956 8.562 0 15.92.638 18.056.919v20.944l-12.399.006c-9.72 0-11.594 4.618-11.594 11.397v14.947h23.193l-3.025 23.42H94.026v65.653c41.476-5.048 73.631-40.312 73.631-83.154 0-46.273-37.532-83.805-83.828-83.805z"
+                                    data-original="#010002"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </form>
             </div>
-        </form>
+            <div class="md:h-full max-md:mt-10 rounded-xl p-2 content-center">
+                <img src="{{ Storage::url('assets/poster.png') }}" class="object-contain rounded-sm hidden md:block"
+                    alt="login-image" />
+                <div class="flex justify-between p-2">
+                    <a href="" class="text-sm text-gray-400 text-center hover:underline">Term & Conditions</a>
+                    <p class="text-sm text-gray-400 text-center">© 2024 Life Core</p>
+                    <a href="" class="text-sm text-gray-400 text-center  hover:underline">Contact</a>
+                </div>
+            </div>
+        </div>
     </x-authentication-card>
 </x-guest-layout>
