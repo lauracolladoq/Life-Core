352b32fdd46203b3a9ca74ff453a056dd20f51c5
lauracolladoq <laucolqui@gmail.com>
2024-05-16 21:07:09 +0200

    Funcionalidad en Login y Register para ver/ocultar campo password

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,46 @@
 import './bootstrap';
 
-// Ocultar/mostrar los comentarios de una publicación
-
-//Seleccionamos todos los botones que tengan un id que empiece por 'toggleComments-'
-document.querySelectorAll('[id^="toggleComments-"]').forEach(function (toggleButton) {
-    //Añadimos un evento 'click' a cada botón
-    toggleButton.addEventListener('click', function () {
-        //Obtenemos el id de la publicación a la que pertenece el botón
-        var postId = this.id.split('-').pop();
-        //Obtenemos el div que contiene los comentarios de la publicación
-        var commentsDiv = document.getElementById('comments-' + postId);
-        //Cambiamos su propiedad 'display' para ocultar/mostrar los comentarios
-        commentsDiv.style.display = commentsDiv.style.display === 'none' ? 'block' : 'none';
+/**
+ * Ver/ocultar los comentarios de una publicación
+ */
+function toggleComments() {
+    //Obtenemos todos los botones que tienen un id que empieza por 'toggleComments-'
+    document.querySelectorAll('[id^="toggleComments-"]').forEach(function (toggleButton) {
+        //Añadimos un evento 'click' a cada botón
+        toggleButton.addEventListener('click', function () {
+            //Obtenemos el id de la publicación a la que pertenece el botón
+            var postId = this.id.split('-').pop();
+            //Obtenemos el div que contiene los comentarios de la publicación
+            var commentsDiv = document.getElementById('comments-' + postId);
+            //Cambiamos su propiedad 'display' para ocultar/mostrar los comentarios
+            commentsDiv.style.display = commentsDiv.style.display === 'none' ? 'block' : 'none';
+        });
     });
-});
+}
+
+toggleComments();
+
+/**
+ * Ver/ocultar el campo password
+ *
+ * @param {string} fieldId - ID del campo de password
+ * @param {string} toggleButtonId - ID del botón que hará la acción
+ */
+function togglePassword(fieldId, toggleButtonId) {
+    // Obtenemos el campo a ver/ocultar y el botón que hará la acción
+    const field = document.getElementById(fieldId);
+    const toggleButton = document.getElementById(toggleButtonId);
+
+    // Añadimos un evento 'click' al botón
+    toggleButton.addEventListener('click', function () {
+        // Cambiamos el tipo del campo de password a text y viceversa
+        if (field.type === 'password') {
+            field.type = 'text';
+        } else {
+            field.type = 'password';
+        }
+    });
+}
+
+togglePassword('password', 'togglePassword');
+togglePassword('password_confirmation', 'toggleConfirmPassword');
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
                         <x-label class="text-xs block mb-2" for="password" value="{{ __('Password') }}" />
                         <div class="relative flex items-center">
                             <x-input id="password" class="w-full text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none" type="password" name="password" required autocomplete="current-password" placeholder="Enter password" />
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
+                            <svg id="togglePassword" xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
                                 <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
                             </svg>
                         </div>
