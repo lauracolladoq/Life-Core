82a1230931c99161d0182b1940d7c656d6515ed4
lauracolladoq <laucolqui@gmail.com>
2024-05-17 19:15:36 +0200

    Diseño modificado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,13 +71,6 @@
                         </div>
                     </a>
                 @endauth
-                <form method="POST" action="{{ route('logout') }}" x-data>
-                    @csrf
-
-                    <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                        {{ __('Log Out') }}
-                    </x-dropdown-link>
-                </form>
                 <!-- -------------------- Fin Perfil -------------------- -->
 
                 <!-- ------------------------------------------- Inicio Aside ------------------------------------------- -->
